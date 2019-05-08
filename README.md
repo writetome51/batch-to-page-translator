eef0e7099187d75bc8e410017491a6f77f45b466
Steve Thompson <writetome51@gmail.com>
2019-05-08 14:51:23 -0600

    Added tests

--- README.md
+++ README.md
@@ -28,16 +28,6 @@ constructor(
 </details>
 
 
-## Properties
-<details>
-<summary>view properties</summary>
-
-```ts
-className : string (read-only)
-    // Not important.  Inherited from BaseClass.
-```
-</details>
-
 ## Methods
 <details>
 <summary>view methods</summary>
@@ -59,59 +49,8 @@ getPageNumberInCurrentBatchFromAbsolutePage(pageNumber): number
     // is 2, and passed `pageNumber` is 11. That would be page 1 of the current 
     // batch, so the function returns 1.
 ```
-The methods below are not important to know about in order to use this  
-class.  They're inherited from [BaseClass](https://github.com/writetome51/typescript-base-class#baseclass) .
-```ts	
-protected   _createGetterAndOrSetterForEach(
-                  propertyNames: string[],
-                  configuration: IGetterSetterConfiguration
-            ) : void
-     /*********************
-     Use this method when you have a bunch of properties that need getter and/or 
-     setter functions that all do the same thing. You pass in an array of string 
-     names of those properties, and the method attaches the same getter and/or 
-     setter function to each property.
-     IGetterSetterConfiguration is this object:
-     {
-         get_setterFunction?: (
-             propertyName: string, index?: number, propertyNames?: string[]
-         ) => Function,
-             // get_setterFunction takes the property name as first argument and 
-             // returns the setter function.  The setter function must take one 
-             // parameter and return void.
-     
-         get_getterFunction?: (
-             propertyName: string, index?: number, propertyNames?: string[]
-         ) => Function
-             // get_getterFunction takes the property name as first argument and 
-             // returns the getter function.  The getter function must return something.
-     }
-     *********************/ 
-   
-   
-protected   _returnThis_after(voidExpression: any) : this
-    // voidExpression is executed, then function returns this.
-    // Even if voidExpression returns something, the returned data isn't used.
-
-
-protected   _errorIfPropertyHasNoValue(
-                property: string, // can contain dot-notation, i.e., 'property.subproperty'
-                propertyNameInError? = ''
-            ) : void
-    // If value of this[property] is undefined or null, it triggers fatal error:
-    // `The property "${propertyNameInError}" has no value.`
-```
 </details>
 
-## Usage Example
-
-```
-
-```
-
-## Inheritance Chain
-
-BatchToPageTranslator<--[BaseClass](https://github.com/writetome51/typescript-base-class#baseclass)
 
 ## Installation
 
--- lib/test.ts
+++ lib/test.ts
@@ -1,4 +1,3 @@
-import { arraysMatch } from '@writetome51/arrays-match';
 import { PaginationPageInfo } from '@writetome51/pagination-page-info';
 import { PaginationBatchInfo } from '@writetome51/pagination-batch-info';
 import { BatchToPageTranslator } from './index';
@@ -33,10 +32,9 @@ else console.log('test 2 FAILED');
 
 // Make sure 1 below minimum value triggers error.
 let errorTriggered = false;
-try{
+try {
 	batchNumber = bch2pgTranslator.getBatchNumberContainingPage(0);
-}
-catch (e) {
+} catch (e) {
 	errorTriggered = true;
 }
 if (errorTriggered) console.log('test 3 passed');
@@ -44,10 +42,9 @@ else console.log('test 3 FAILED');
 
 // Make sure 1 above maximum value triggers error.
 errorTriggered = false;
-try{
+try {
 	batchNumber = bch2pgTranslator.getBatchNumberContainingPage(6);
-}
-catch (e) {
+} catch (e) {
 	errorTriggered = true;
 }
 if (errorTriggered) console.log('test 4 passed');
@@ -55,13 +52,150 @@ else console.log('test 4 FAILED');
 
 // Make sure negative number triggers error.
 errorTriggered = false;
-try{
+try {
 	batchNumber = bch2pgTranslator.getBatchNumberContainingPage(-1);
-}
-catch (e) {
+} catch (e) {
 	errorTriggered = true;
 }
 if (errorTriggered) console.log('test 5 passed');
 else console.log('test 5 FAILED');
 
 
+// Now test the same function using different values of data total, items per page and items per batch:
+
+dataSource.dataTotal = 1021;
+pageInfo.itemsPerPage = 13;
+batchInfo.itemsPerBatch = 26;
+
+// Make sure the minimum value gets correct result:
+batchNumber = bch2pgTranslator.getBatchNumberContainingPage(1);
+if (batchNumber === 1) console.log('test 6 passed');
+else console.log('test 6 FAILED');
+
+// Make sure the maximum value gets correct result:
+batchNumber = bch2pgTranslator.getBatchNumberContainingPage(79);
+if (batchNumber === 40) console.log('test 7 passed');
+else console.log('test 7 FAILED');
+
+// Make sure 1 below minimum value triggers error.
+errorTriggered = false;
+try {
+	batchNumber = bch2pgTranslator.getBatchNumberContainingPage(0);
+} catch (e) {
+	errorTriggered = true;
+}
+if (errorTriggered) console.log('test 8 passed');
+else console.log('test 8 FAILED');
+
+// Make sure 1 above maximum value triggers error.
+errorTriggered = false;
+try {
+	batchNumber = bch2pgTranslator.getBatchNumberContainingPage(80);
+} catch (e) {
+	errorTriggered = true;
+}
+if (errorTriggered) console.log('test 9 passed');
+else console.log('test 9 FAILED');
+
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(1);
+if (batchInfo.currentBatchNumber === 1) console.log('test 10 passed');
+else console.log('test 10 FAILED');
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(31);
+if (batchInfo.currentBatchNumber === 16) console.log('test 11 passed');
+else console.log('test 11 FAILED');
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(79);
+if (batchInfo.currentBatchNumber === 40) console.log('test 12 passed');
+else console.log('test 12 FAILED');
+
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(78);
+if (bch2pgTranslator.currentBatchContainsPage(78)) console.log('test 13 passed');
+else console.log('test 13 FAILED');
+
+if (bch2pgTranslator.currentBatchContainsPage(79)) console.log('test 14 FAILED');
+else console.log('test 14 passed');
+
+
+errorTriggered = false;
+try {
+	bch2pgTranslator.currentBatchContainsPage(80);
+} catch (e) {
+	errorTriggered = true;
+}
+if (errorTriggered) console.log('test 15 passed');
+else console.log('test 15 FAILED');
+
+
+batchInfo.currentBatchNumber = undefined;
+if (bch2pgTranslator.currentBatchContainsPage(78)) console.log('test 16 FAILED');
+else console.log('test 16 passed');
+
+
+// Now test method getPageNumberInCurrentBatchFromAbsolutePage():
+
+errorTriggered = false;
+try {
+	bch2pgTranslator.getPageNumberInCurrentBatchFromAbsolutePage(1);
+} catch (e) {
+	errorTriggered = true;
+}
+if (errorTriggered) console.log('test 17 passed');
+else console.log('test 17 FAILED');
+
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(1);
+let pageNum = bch2pgTranslator.getPageNumberInCurrentBatchFromAbsolutePage(1);
+if (pageNum === 1) console.log('test 18 passed');
+else console.log('test 18 FAILED');
+
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(5);
+pageNum = bch2pgTranslator.getPageNumberInCurrentBatchFromAbsolutePage(5);
+if (pageNum === 1) console.log('test 19 passed');
+else console.log('test 19 FAILED');
+
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(6);
+pageNum = bch2pgTranslator.getPageNumberInCurrentBatchFromAbsolutePage(6);
+if (pageNum === 2) console.log('test 20 passed');
+else console.log('test 20 FAILED');
+
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(78);
+pageNum = bch2pgTranslator.getPageNumberInCurrentBatchFromAbsolutePage(78);
+if (pageNum === 2) console.log('test 21 passed');
+else console.log('test 21 FAILED');
+
+
+// Now test the same function using different values of items per page and items per batch:
+
+dataSource.dataTotal = 1021;
+pageInfo.itemsPerPage = 20;
+batchInfo.itemsPerBatch = 100;
+
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(1);
+pageNum = bch2pgTranslator.getPageNumberInCurrentBatchFromAbsolutePage(1);
+if (pageNum === 1) console.log('test 22 passed');
+else console.log('test 22 FAILED');
+
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(25);
+pageNum = bch2pgTranslator.getPageNumberInCurrentBatchFromAbsolutePage(25);
+if (pageNum === 5) console.log('test 23 passed');
+else console.log('test 23 FAILED');
+
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(26);
+pageNum = bch2pgTranslator.getPageNumberInCurrentBatchFromAbsolutePage(26);
+if (pageNum === 1) console.log('test 24 passed');
+else console.log('test 24 FAILED');
+
+
+bch2pgTranslator.set_currentBatchNumber_toBatchContainingPage(52);
+pageNum = bch2pgTranslator.getPageNumberInCurrentBatchFromAbsolutePage(52);
+if (pageNum === 2) console.log('test 25 passed');
+else console.log('test 25 FAILED');
